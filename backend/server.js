c0cee964fd17e19f20a11a6138bfb71848fdfed2
kajanagur123 <167634431+kajanagur123@users.noreply.github.com>
2025-09-08 23:52:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-/const express = require("express");
+const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -14,6 +14,8 @@ app.use("/api", studentRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => { console.log("MongoDB connected"); app.listen(PORT, () => console.log("Server running on port " + PORT)); })
+  .then(() => { 
+    console.log("MongoDB connected"); 
+    app.listen(PORT, () => console.log("Server running on port " + PORT)); 
+  })
   .catch(err => console.error(err));
-
